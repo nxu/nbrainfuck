da5ce5d2bbe089df5c65b69f8777e2f2cb32d428
nXu <nabunub@hotmail.com>
2014-01-14 18:00:36 +0100

    Included license information and renamed Brainfuck Developer to BFIDE (the name already exists)

--- BFIDE/AboutWindow.xaml.cs
+++ BFIDE/AboutWindow.xaml.cs
@@ -15,14 +15,9 @@
             Version v = asmname.Version;
 
             this.TBXVersion.Text = string.Format("Version {0}", v);
-            this.TBXInfo.Text = 
-                "nBrainfuck is a collection of freeware " +
-                "and partly open source softwares written by nXu. Brainfuck Developer (a Brainfuck IDE) is a " +
-                "part of it, so all licenses and terms of nBrainfuck apply to it.\n" +
-                "TLDR: Feel free to use and share the software - for free. Have fun with it! ;)\n\n" +
-                "Brainfuck Developer uses AvalonEdit for code editor box:\n" +
-                "http://www.codeproject.com/Articles/42490/Using-AvalonEdit-WPF-Text-Editor \n\n" + 
-                "(c) 2012 nXu - http://brainfuck.nXu.hu";
+            this.TBXInfo.Text = "BFIDE is licensed under The MIT License. \n\n" + Properties.Resources.MITLicense +
+                "\n\nBrainfuck IDE uses AvalonEdit as the code editor box, which is licensed under LGPL License:\n" +
+                "https://github.com/icsharpcode/SharpDevelop/wiki/AvalonEdit \n\n";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
--- BFIDE/Properties/AssemblyInfo.cs
+++ BFIDE/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BFIDE")]
+[assembly: AssemblyTitle("nbrainfuck BFIDE")]
 [assembly: AssemblyDescription("A free integrated development environment for brainfuck programmers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("nXu")]
 [assembly: AssemblyProduct("BFIDE")]
-[assembly: AssemblyCopyright("Copyright © nXu 2012")]
+[assembly: AssemblyCopyright("Copyright © nXu 2012 - 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.*")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.*")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
